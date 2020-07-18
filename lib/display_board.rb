4279a27b6ffcd74d7c9b6a61bd724c7706df86dc
Valerie Nuccio <val.nuccio@gmail.com>
2020-07-18 19:05:06 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,6 +3,7 @@ board_array=["","","","","","","","",""]
 def board (board_strings)
   puts "#{board_array[0]}|#{board_array[1]}|#{board_array[2]}"
   puts "-----------"
-  puts ""
+  puts "#{board_array[3]}|#{board_array[4]}|#{board_array[5]"
+  puts "
 end# Define display_board that accepts a board and prints
 # out the current state.
