adab72ee72ca6819b47ad323dcc698cc831124ef
Valerie Nuccio <val.nuccio@gmail.com>
2020-07-18 19:22:50 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,4 @@
-def display_board(board)
+def display_board(board1)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
@@ -6,5 +6,5 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-board_example= 
+board=["x","x","x",]
 display_board(board_example)
