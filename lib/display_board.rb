a183e1181991fa48df6e0ec654a6c95fa9c5bbaf
Valerie Nuccio <val.nuccio@gmail.com>
2020-07-18 19:11:08 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 board_array=["","","","","","","","",""]
 
 def board (board)
-  puts board_array[0]|board_array[1]|board_array[2]
+  puts #{board_array[0]}|#{board_array[1]}|#{board_array[2]}
   puts "-----------"
   puts "#{board_array[3]}|#{board_array[4]}|#{board_array[5]}"
   puts "-----------"
